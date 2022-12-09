be20417828da0d9b600cd25f28a86b16d7a6afad
groszyk.lukasz@gmail.com <groszyk.lukasz@gmail.com>
2022-12-09 12:13:37 +0100

    adding human class

--- src/classes/human.py
+++ src/classes/human.py
@@ -1,4 +1,6 @@
-class Human(Character):
+import numpy as np
+import common
+class Human(common.Character):
     def __init__(self, x, y, velocity, power, n_killed=0):
         super().__init__(x, y, velocity, power)
         self.n_killed = n_killed
@@ -11,6 +13,19 @@ class Human(Character):
                f"n_killed={self.n_killed})"
 
     def choose_new_position(self, zombies):  # zombies ->  [ Zombie(), Zombie(), Zombie()   ]
-        delta_x = random.random() * self.velocity
-        delta_y = random.random() * self.velocity
+
+        vector_to_zombies = [np.array([z.x - self.x, z.y - self.y]) for z in zombies]
+        vector_to_zombies_norm = [v / (np.linalg.norm(v) + 0.0000001) for v in vector_to_zombies]
+        weighted_vector = [vn * (self.power + self.killed) - vn * (z.power + z.n_infected)
+                           for vn, z in zip(vector_to_zombies_norm, zombies)] #przeiterowanie po wszystkich zombie i wszystkich wectorach
+
+        delta_x = 2 * np.random.random() -1
+        delta_y = (1 - delta_x ** 2) ** 0.5 * (1 if np.random.random() > 0.5 else -1)
+
+        delta_x *= self.velocity
+        delta_y *= self.velocity
+
         return delta_x, delta_y
+
+human = Human(0, 0, 5, 5)
+zombies = [(10, 12), (12,14)]
